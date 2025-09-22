9dc55a81a4461416c9e32f93589978e66a8e94c2
Chene2 <buenchen2@gmail.com>
2025-09-22 20:34:35 +0800

    Update App.java

--- src/App.java
+++ src/App.java
@@ -3,7 +3,10 @@ public class App{
 
         //Add your name to confirm you have access to this source code
         System.out.println("Noel is online");
+        System.out.println("Hello, Kalibutan");
+        System.out.println("Chelsey is here");
         System.out.println("Rem is online");
+
         
     }
 }
