aa80f040f1cc296a8d41a9c4c5a76cb55bcf0a99
Noel Arcenio Cortes <166308456+NoelPCortes@users.noreply.github.com>
2025-09-25 21:31:17 +0800

    added reusable codes, Character skills and improved Character stats

--- src/CharacterSkills.java
+++ src/CharacterSkills.java
@@ -4,5 +4,23 @@
  */
 
 public class CharacterSkills {
-    
+    private String nameOfSkill;
+    private int dmgOfSkill;
+    private int cooldownOfSkill;
+    private int manaConsumedOfSkill;
+
+    public CharacterSkills(){
+        nameOfSkill = "Unknown";
+        dmgOfSkill = 0;
+        cooldownOfSkill = 0;
+        manaConsumedOfSkill = 0;
+    }
+
+    public CharacterSkills(String nameOfSkill, int dmgOfSkill,
+                            int cooldownOfSkill, int manaConsumedOfSkill){
+        this.nameOfSkill = nameOfSkill;
+        this.dmgOfSkill = dmgOfSkill;
+        this.cooldownOfSkill = cooldownOfSkill;
+        this.manaConsumedOfSkill = manaConsumedOfSkill;    
+    }
 }
--- src/CharacterStats.java
+++ src/CharacterStats.java
@@ -1,13 +1,11 @@
 public class CharacterStats {
-
     private String name;
     private String role;
     private int health;
     private int mana;
     private boolean isLocked;
 
-    public CharacterStats()
-    {
+    public CharacterStats(){
         name = "Unknown";
         role = "Unknown";
         health = 0;
@@ -15,8 +13,7 @@ public class CharacterStats {
         isLocked = true;
     }
 
-    public CharacterStats(String name, String role, int health, int mana, boolean isLocked)
-    {
+    public CharacterStats(String name, String role, int health, int mana, boolean isLocked){
         this.name = name;
         this.role = role;
         this.health = health;
@@ -35,13 +32,29 @@ public class CharacterStats {
     int getHealth(){
         return health;
     }
+    void setHealth(int health){
+        this.health = health;
+    }
 
     int getMana(){
         return mana;
     }
+    void setMana(int mana){
+        this.mana = mana;
+    }
 
-    boolean getIsLocked(){
-        return isLocked;
+    String getIsLocked(){
+        return (isLocked) ? "[Locked]" : "";
+    }
+
+    void printCharacterStats(){
+        System.out.println("--------------------------------");
+        System.out.println("        Name: " + name);
+        System.out.println("        Role: " + role);
+        System.out.println("        Health: " + health);
+        System.out.println("        Mana: " + mana);
+        System.out.println("--------------------------------");
+        System.out.println();
     }
 }
 
@@ -49,16 +62,39 @@ class ChelseyStats extends CharacterStats{
     public ChelseyStats(boolean isLocked){// Fill the parameters
         super("idk", "idk", 10, 10, isLocked);
     }
+
+    @Override
+    void printCharacterStats() {
+        super.printCharacterStats();
+        //fill this part
+    }
 }
 
 class NoelStats extends CharacterStats{
     public NoelStats(String name, String role, int health, int  mana, boolean isLocked){
         super(name, role, health, mana, isLocked);
     }
+
+    @Override
+    void printCharacterStats() {
+        System.out.println("--------------------------------");
+        System.out.println("        Name: " + getName());
+        System.out.println("        Role: " + getRole());
+        System.out.println("        Health: " + getHealth());
+        System.out.println("        Mana: " + getMana());
+        System.out.println("--------------------------------");
+        System.out.println();
+    }
 }
 
 class RemrieStats extends CharacterStats{
     public RemrieStats(){// Fill the parameters
         super();
+    }
+
+    @Override
+    void printCharacterStats() {
+        super.printCharacterStats();
+        //fill this part
     }
 }
